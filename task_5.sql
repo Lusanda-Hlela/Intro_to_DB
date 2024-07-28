e668f4549673a910d491093b092289eb2fadff60
Lusanda <hlelalusa@gmail.com>
2024-07-28 09:37:38 +0200

    Updated versions

--- task_5.sql
+++ task_5.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS alx_book_store
 -- Use the database passed as an argument
 USE alx_book_store;
 
--- Insert a single row into the Customers table
-INSERT INTO Customer (customer_id, customer_name, email, address)
+-- Insert a single row into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
