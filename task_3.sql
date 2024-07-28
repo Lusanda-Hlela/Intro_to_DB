b5541272a1da33187c809c5aaaa13dee8d853c13
Lusanda <hlelalusa@gmail.com>
2024-07-28 09:01:21 +0200

    List all the tables created

---
+++ task_3.sql
@@ -0,0 +1,7 @@
+-- Use the database passed as an argument
+USE alx_book_store;
+
+-- List all tables in the specified database
+SELECT table_name 
+FROM information_schema.tables 
+WHERE table_schema = 'alx_book_store';
